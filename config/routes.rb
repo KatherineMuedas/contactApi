6e683556f695a1bd1052351f758e6b7934f4f387
katherine <muedasnka@gmail.com>
2015-10-11 21:07:36 -0600

    add api inflection and change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  namespace :api, :defaults => {:format => :json } do
+  namespace :api, defaults: {format: :json } do
     namespace :v1 do 
-      get "/contacts", to: "contacts#index" 
-      post "/contacts", to: "contacts#create" 
-      get "/contacts/ :id", to: "contacts#show" 
-      put "/contacts/ :id", to: "contacts#update" 
-      delete "/contacts/ :id", to: "contacts#destroy" 
+      resources :contacts, except: [:new, :edit]
+      # get "/contacts", to: "contacts#index" 
+      # post "/contact", to: "contact#create" 
+      # get "/contact/:id", to: "contact#show" 
+      # put "/contact/:id", to: "contact#update" 
+      # delete "/contact/:id", to: "contact#destroy" 
     end
   end
 end
