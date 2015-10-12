b3f5b5fe5cfe2e81d79f47b0fa20cc2e8406c3be
katherine <muedasnka@gmail.com>
2015-10-11 18:18:24 -0600

    adds versioning and contact controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api, :defaults => {:format => :json } do 
-    get "/contacts", to: "contacts#index" 
-    post "/contacts", to: "contacts#create" 
-    get "/contacts/ :id", to: "contacts#show" 
-    put "/contacts/ :id", to: "contacts#update" 
-    delete "/contacts/ :id", to: "contacts#destroy" 
+  namespace :api, :defaults => {:format => :json } do
+    namespace :v1 do 
+      get "/contacts", to: "contacts#index" 
+      post "/contacts", to: "contacts#create" 
+      get "/contacts/ :id", to: "contacts#show" 
+      put "/contacts/ :id", to: "contacts#update" 
+      delete "/contacts/ :id", to: "contacts#destroy" 
+    end
   end
 end
